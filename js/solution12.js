a143c38598885b27a0a342abddf6186334610032
Keevah9 <keevahhphp@gmail.com>
2022-05-21 10:27:01 +0100

    short long short

--- js/solution12.js
+++ js/solution12.js
@@ -1,13 +1,12 @@
-// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 
-// Hint for R users:
+Hint for R users:
 
-// The length of string is not always the same as the number of characters
-// For example: (Input1, Input2) --> output
+The length of string is not always the same as the number of characters
+For example: (Input1, Input2) --> output
 
-// ("1", "22") --> "1221"
-// ("22", "1") --> "1221"
-
+("1", "22") --> "1221"
+("22", "1") --> "1221"
 
 
 
@@ -16,3 +15,5 @@ function solution(a, b){
   let shrt = a.length < b.length ? a : b
   let long = a.length > b.length ? a : b
   return shrt + long + shrt
+}
+
